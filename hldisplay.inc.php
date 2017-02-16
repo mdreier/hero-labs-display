6a8caf44a1c624aef6d99f6b08440d96fb7b4fb2
Martin Dreier <martin.dreier@sap.com>
2017-02-16 16:27:40 +0100

    Move DropPHP include into include file

--- hldisplay.inc.php
+++ hldisplay.inc.php
@@ -1,4 +1,5 @@
 <?php
+require_once("DropPHP/DropboxClient.php");
 
 /**
  * Hero Labs Display class.
--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 session_start();
 
-require_once("DropPHP/DropboxClient.php");
 require_once("config.php");
 require_once("hldisplay.inc.php");
 

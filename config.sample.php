ac34a0904d1cb138811ae55f00845ae426f1173b
Martin Dreier <martin.dreier@sap.com>
2017-02-17 17:01:53 +0100

    Improve documentation in sample configuration file

--- config.sample.php
+++ config.sample.php
@@ -24,19 +24,44 @@ SOFTWARE.
 */
 $config = array(
 
-	//Set the Dropbox app key and app secret
+	/*
+	 Set the Dropbox API application key and secret. You can get these
+	 by registering this app on the Dropbox developer portal:
+	 https://www.dropbox.com/developers/apps/create
+	*/
 	'dropbox_app_key'		=> "<app key>",
 	'dropbox_app_secret'	=> "<app secret>",
 
-	//Proxy configuration
+	/*
+	 Proxy configuration.
+	 If your server must use a proxy to connect to the Dropbox servers
+	 configure it here. If no proxy is set direct connection is used.
+	 Use 'proxy_user' and 'proxy_password' if your proxy requires
+	 authentication.
+	*/
 	'proxy_url'				=> '',
 	'proxy_user'			=> '',
 	'proxy_password'		=> '',
 
-	//Path to the working directory for caches and tokens
+	/*
+	 Path to the working directory for caches and tokens. For security reasons
+	 this directory should not be reachable externally.
+	*/
 	'working_directory' 	=> dirname(__FILE__) . '/work',
 
-	//Search parameters
+	/*
+	 Search parameters.
+	 'search_path' is the path inside Dropbox which is searched for portfolios.
+	               The default value is the path used for Hero Lab on iPad. You
+								 can add any number of paths here. To search the whole Dropbox
+								 directory, set it to '/', but be aware that this will cause a
+								 dramaticperformance decrease depending on the number of files
+								 in your dropbox.
+	'result_with_path' determines if the path of the portfolio in Dropbox is
+	                   shown in the list of portfolios. If set to true, the path
+										 is shown. If set to false, only the portfolio name (i.e.
+										 the file name) is shown in the list.
+	*/
 	'result_size'			=> 25,
 	'search_path'			=> array('/Apps/Hero Lab'),
 	'result_with_path' => false
